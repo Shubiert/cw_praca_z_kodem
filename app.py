21b7b1034d9dbed1b994601fe2404cdf137e5255
Shubiert <91896973+Shubiert@users.noreply.github.com>
2023-07-02 14:56:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Strona główna
+
+    Funkcja obsługuje żądania na stronie głównej.
+    Zwraca HTML z napisem "Hello WSB! Greetings from Flask!".
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
